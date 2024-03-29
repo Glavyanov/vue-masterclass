f36b7495f947f4925df797b58c652d0f471ae1ff
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2024-03-29 13:41:40 +0200

    Using Multiple Rules on a Single Field install vee-validate-rules package.

--- src/plugins/VeeValidatePlugin.js
+++ src/plugins/VeeValidatePlugin.js
@@ -1,11 +1,14 @@
 import { Field, Form, ErrorMessage, defineRule } from "vee-validate"
+import { email } from "@vee-validate/rules";
 
 export default (app) => {
 
     defineRule('required', (value, _, el) => {
         if(value && value.trim()) return true;
         return `${el?.label || 'This'} is required`;
-    })
+    });
+
+    defineRule('email', email);
 
     app.component('VeeForm', Form);
     app.component('VeeField', Field);
