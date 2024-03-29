7b5109dac1a86ee2f524b3d93f77d39ee4107626
Dimitar Glavyanov <Dimitar.Glavyanov@kontrax.bg>
2024-03-29 14:01:23 +0200

    Customizing VeeValidate Error Messages

--- src/plugins/VeeValidatePlugin.js
+++ src/plugins/VeeValidatePlugin.js
@@ -1,15 +1,22 @@
-import { Field, Form, ErrorMessage, defineRule } from "vee-validate"
-import { email, min } from "@vee-validate/rules";
+import { Field, Form, ErrorMessage, defineRule, configure } from "vee-validate"
+import { email, min, required } from "@vee-validate/rules";
+import { localize } from "@vee-validate/i18n";
 
 export default (app) => {
 
-    defineRule('required', (value, _, el) => {
-        if(value && value.trim()) return true;
-        return `${el?.label || 'This'} is required`;
-    });
-
+    defineRule('required', required);
     defineRule('email', email);
     defineRule('min', min);
+
+    configure({
+        generateMessage: localize('bg', {
+            messages: {
+                required: 'Полето {field} е задължително',
+                email: 'Полето {field} трябва да е коректен имейл адрес',
+                min: 'Полето {field} трябва да съдържа минимум 0:{min} символа',
+            }
+        })
+    })
 
     app.component('VeeForm', Form);
     app.component('VeeField', Field);
